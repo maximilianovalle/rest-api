254ab2a93c8cadebae0d718877cffe3174c06f1e
maximilianovalle <109977231+maximilianovalle@users.noreply.github.com>
2025-07-27 16:08:48 -0500

    Create POST method for new book instance

--- Controllers/BooksController.cs
+++ Controllers/BooksController.cs
@@ -54,8 +54,16 @@ namespace proj1.Controllers
             var book = books.FirstOrDefault(x => x.ID == id);
 
             if (book == null) return NotFound();
-
             return Ok(book);
+        }
+
+        [HttpPost]
+        public ActionResult<Book> AddBook(Book newBook)
+        {
+            if (newBook == null) return BadRequest();
+
+            books.Add(newBook);
+            return CreatedAtAction(nameof(GetBookByID), new {id = newBook.ID}, newBook);
         }
     }
 }
--- obj/Debug/net9.0/proj1.AssemblyInfo.cs
+++ obj/Debug/net9.0/proj1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("proj1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf11306ee439f1d6b456a4475e9d977c5e36fa56")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2798bc968643908a579cd3eb4f110ff318b01323")]
 [assembly: System.Reflection.AssemblyProductAttribute("proj1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("proj1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
